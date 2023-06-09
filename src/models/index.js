3e578c8b48965e0e42441a68a273da6079db9f3c
RNTVNisNhut <ntn2k3kg@gmail.com>
2023-06-09 10:13:17 +0700

    3 times

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,14 @@
-const FoodModel = require("./Collections/food.model");
-const FoodTypeModel = require("./Collections/foodtype.model");
-const DiscountModel = require("./Collections/discount.model");
-const OrderModel = require("./Collections/order.model");
-const OrderDetailModel = require("./Collections/orderdetail.model");
-const PaymentModel = require("./Collections/payment.model");
-const RatingModel = require("./Collections/rating.model");
-const ShipperModel = require("./Collections/shipper.model");
-const ShippingModel = require("./Collections/shipping.model");
-const ShopModel = require("./Collections/shop.model");
-const SiteModel = require("./Collections/site.model");
+const FoodModel = require("./collections/food.model");
+const FoodTypeModel = require("./collections/foodtype.model");
+const DiscountModel = require("./collections/discount.model");
+const OrderModel = require("./collections/order.model");
+const OrderDetailModel = require("./collections/orderdetail.model");
+const PaymentModel = require("./collections/payment.model");
+const RatingModel = require("./collections/rating.model");
+const ShipperModel = require("./collections/shipper.model");
+const ShippingModel = require("./collections/shipping.model");
+const ShopModel = require("./collections/shop.model");
+const SiteModel = require("./collections/site.model");
 const KeyTokenModel = require("./collections/keytoken.model");
 
 
